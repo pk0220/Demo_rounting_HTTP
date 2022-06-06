30eeb1a6ffb9edce5c9851652cbbe3df02fe3230
pk0220 <pratikkhamkar220@gmail.com>
2022-06-06 09:42:52 +0530

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo_rounting_HTTP.Models
+{
+    public class Student
+    {
+        public int studentId { get; set; }
+        public string studentName { get; set; }
+    }
+}
